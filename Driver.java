c3ce17db02d32b571a8ea5a9a8e4277cd5c4aa72
Yong Le <ygle.he@gmail.com>
2023-06-06 20:34:45 -0400

    modified existing functions to accomodate new class structure

--- Driver.java
+++ Driver.java
@@ -1,16 +1,33 @@
 public class Driver {
   public static void main(String[] args) {
-    Polynomial p = new Polynomial();
-    System.out.println(p.evaluate(3));
-    double[] c1 = { 6, 0, 0, 5 };
-    Polynomial p1 = new Polynomial(c1);
-    double[] c2 = { 0, -2, 0, 0, -9 };
-    Polynomial p2 = new Polynomial(c2);
-    Polynomial s = p1.add(p2);
-    System.out.println("s(0.1) = " + s.evaluate(0.1));
-    if (s.hasRoot(1))
-      System.out.println("1 is a root of s");
-    else
-      System.out.println("1 is not a root of s");
+    // Polynomial p = new Polynomial();
+    // System.out.println(p.evaluate(3));
+    // double[] c1 = { 6, 0, 0, 5 };
+    // Polynomial p1 = new Polynomial(c1);
+    // double[] c2 = { 0, -2, 0, 0, -9 };
+    // Polynomial p2 = new Polynomial(c2);
+    // Polynomial s = p1.add(p2);
+    // System.out.println("s(0.1) = " + s.evaluate(0.1));
+    // if (s.hasRoot(1))
+    //   System.out.println("1 is a root of s");
+    // else
+    //   System.out.println("1 is not a root of s");
+
+    double[] c1 = {5, 3, 7};
+    double[] c2 = {6, -2, 5};
+    int[] a = {0, 2, 8};
+    int[] b = {0, 1, 3};
+    Polynomial f = new Polynomial(c1, a);
+    Polynomial g = new Polynomial(c2, b);
+    double[] c = f.add(g).coefficients;
+    for (double i : c) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+    int[] d = f.add(g).exponents;
+    for (double i : d) {
+      System.out.print(i + " ");
+    }
+
   }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,35 +1,72 @@
+import java.io.File;
+
 public class Polynomial {
   double coefficients[];
+  int exponents[];
 
   public Polynomial() {
     coefficients = new double[1];
+    exponents = new int[1];
   }
 
-  public Polynomial(double arg[]) {
-    coefficients = arg;
+  public Polynomial(double coeff[], int exp[]) {
+    coefficients = coeff;
+    exponents = exp;
   }
+
+  public Polynomial(File file) {}
 
   public Polynomial add(Polynomial toAdd) {
-    int length = Math.max(this.coefficients.length, toAdd.coefficients.length);
+    int length = this.getDistinctExponents(toAdd);
 
-    double newCoefficients[] = new double[length];
+    // Need to be calling these a lot
+    int selfLength = coefficients.length;
+    int otherLength = toAdd.coefficients.length;
 
+    double[] newCoeff = new double[length];
+    int[] newExp = new int[length];
+
+    // Used to keep track of which term we are at for each poly
+    int selfIndex = 0;
+    int otherIndex = 0;
+
+    // Add each term from smallest exponent to largest exponent
     for (int i = 0; i < length; i++) {
-      if (i < coefficients.length) {
-        newCoefficients[i] += this.coefficients[i];
-      }
-      if (i < toAdd.coefficients.length) {
-        newCoefficients[i] += toAdd.coefficients[i];
+
+      // When exponents of both terms are the same
+      if (selfIndex < selfLength
+          && otherIndex < otherLength
+          && exponents[selfIndex] == toAdd.exponents[otherIndex]) {
+        newCoeff[i] = coefficients[selfIndex] + toAdd.coefficients[otherIndex];
+        newExp[i] = exponents[selfIndex];
+        selfIndex++;
+        otherIndex++;
+
+        // Case 1: When we finish adding all terms from calling object
+        // Case 2: When argument object has the smaller exponent
+      } else if (selfIndex >= selfLength
+          || otherIndex < otherLength && exponents[selfIndex] > toAdd.exponents[otherIndex]) {
+        newCoeff[i] = toAdd.coefficients[otherIndex];
+        newExp[i] = toAdd.exponents[otherIndex];
+        otherIndex++;
+
+        // Case 1: When we finish adding all terms from argument object
+        // Case 2: When
+      } else if (otherIndex >= otherLength
+          || selfIndex < selfLength && exponents[selfIndex] < toAdd.exponents[otherIndex]) {
+        newCoeff[i] = coefficients[selfIndex];
+        newExp[i] = exponents[selfIndex];
+        selfIndex++;
       }
     }
 
-    return new Polynomial(newCoefficients);
+    return new Polynomial(newCoeff, newExp);
   }
 
   public double evaluate(double x) {
     double ans = 0.0;
     for (int i = 0; i < coefficients.length; i++) {
-      ans += coefficients[i] * Math.pow(x, i);
+      ans += coefficients[i] * Math.pow(x, exponents[i]);
     }
     return ans;
   }
@@ -38,4 +75,35 @@ public class Polynomial {
     return this.evaluate(x) == 0;
   }
 
+  public Polynomial multiply(Polynomial toMult) {
+    // Idea: Create array of self.length x toMult.length and use
+    // 2 nested for loops to multiply coefficients and add exponents
+    // Add helper function to collapse the array into a smaller array
+    return new Polynomial();
+  }
+
+  public void saveToFile(File file) {}
+
+  // Returns the number of distinct exponents between the calling
+  // object and argument object
+  private int getDistinctExponents(Polynomial other) {
+    int distinct = exponents.length;
+    boolean isUnique = true;
+    for (int i : other.exponents) {
+      isUnique = true;
+      for (int j : exponents) {
+        if (i == j) {
+          isUnique = false;
+          break;
+        }
+      }
+      if (isUnique) distinct += 1;
+    }
+
+    return distinct;
+  }
+
+  private void addToIndex(int[] arr, int index, int value) {
+    arr[index] += value;
+  }
 }
